a7949ce7f560c3074f4f2440c1c298f785e22bff
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-02-19 14:04:23 -0300

    inserting button element

--- Hello World/Hello World/MainPage.xaml.cs
+++ Hello World/Hello World/MainPage.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Hello_World
@@ -13,6 +8,11 @@ namespace Hello_World
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Hello", "Hello World!", "OK");
         }
     }
 }
